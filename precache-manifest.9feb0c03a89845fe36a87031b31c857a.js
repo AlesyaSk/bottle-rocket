56cf233801a739fe5dcd101a2968b8180a5a5be1
AlesyaSk <a.skorohod@ydt-global.com>
2020-07-30 17:26:47 +0300

    Updates

---
+++ precache-manifest.9feb0c03a89845fe36a87031b31c857a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa58fd0ea5ceee3409006f35b8e59828",
+    "url": "/bottle-rocket/index.html"
+  },
+  {
+    "revision": "0cf046d3ac1156a73718",
+    "url": "/bottle-rocket/static/css/main.e16aaf12.chunk.css"
+  },
+  {
+    "revision": "65daacdb7444408714bc",
+    "url": "/bottle-rocket/static/js/2.2a7f1ee2.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/bottle-rocket/static/js/2.2a7f1ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf046d3ac1156a73718",
+    "url": "/bottle-rocket/static/js/main.7c9eba49.chunk.js"
+  },
+  {
+    "revision": "3234aad8039ec4b2bc1d",
+    "url": "/bottle-rocket/static/js/runtime-main.355b8dd9.js"
+  },
+  {
+    "revision": "88da9ccecc9a384876ccf7c53eccec2d",
+    "url": "/bottle-rocket/static/media/AvenirNext-Bold.88da9cce.ttf"
+  },
+  {
+    "revision": "2708c5a2686355c1c1ed9b31454e7906",
+    "url": "/bottle-rocket/static/media/AvenirNext-Demi.2708c5a2.ttf"
+  },
+  {
+    "revision": "1f1c6c5055bfc41663d1d1adf4b29604",
+    "url": "/bottle-rocket/static/media/AvenirNext-Regular.1f1c6c50.ttf"
+  },
+  {
+    "revision": "cedc6ec6806b01a8ccba6e3de1e408dd",
+    "url": "/bottle-rocket/static/media/pin.cedc6ec6.png"
+  }
+]);
